e4e845a75e10c65155b54836d27efa1ad39bf35e
RegisVavasseur <vavasseu2u@etu.univ-lorraine.fr>
2024-03-26 16:15:13 +0100

    Ajout du code pour la page IsConnected.vue pour le middleware afin de sécuriser la route

--- pages/IsConnected.vue
+++ pages/IsConnected.vue
@@ -1,28 +1,54 @@
 <template>
-    <div>
-      <h1>{{user.value }}</h1>
-      <button type="submit" onclick="logout"> Se déconnecter</button>
-    </div>
-  </template>
-  
-  <script setup>
-  
-  
-  </script>
-  
-  <style>
-  h1 {
-    color: #28b463;
-    font-size: 3rem;
-    font-weight: bold;
-    text-align: center;
-    margin: 2rem 0;
-  }
+  <div>
+    <h1>{{ user }}</h1>
+    <p>Vous êtes connecté</p>
+    <p>Vous pouvez vous déconnecter en cliquant sur le bouton ci-dessous</p>
+    <button type="submit" @click="logout"> Se déconnecter</button>
+  </div>
+</template>
 
-  button{
-    background-color: #28b463;
-    color: antiquewhite;
-    padding: 10px;
-    border-radius: 5px;
+<script setup>
+
+
+
+const router = useRouter();
+
+
+
+
+definePageMeta({
+  middleware(to, from) {
+    function getUser() {
+      return localStorage.getItem('user')
+    }
+    // Récupérer l'utilisateur connecté du localStorage
+    const user = getUser()
+
+    if (!user) {
+      return navigateTo('/');
+    }
   }
-  </style>
+});
+
+function logout() {
+  localStorage.removeItem('user');
+  router.push('/');
+}
+</script>
+
+<style>
+h1 {
+  color: #28b463;
+  font-size: 3rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 2rem 0;
+}
+
+button {
+  background-color: #28b463;
+  color: antiquewhite;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
