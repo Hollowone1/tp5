26345dce7bac465fe98d4f57abc9a54e1d3a6f84
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-26 15:48:36 +0100

    isConected style

--- pages/IsConnected.vue
+++ pages/IsConnected.vue
@@ -1,6 +1,28 @@
 <template>
+    <div>
+      <h1>{{user.value }}</h1>
+      <button type="submit" onclick="logout"> Se déconnecter</button>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  
+  </script>
+  
+  <style>
+  h1 {
+    color: #28b463;
+    font-size: 3rem;
+    font-weight: bold;
+    text-align: center;
+    margin: 2rem 0;
+  }
 
-</template>
-
-<script>
-</script>
+  button{
+    background-color: #28b463;
+    color: antiquewhite;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  </style>
