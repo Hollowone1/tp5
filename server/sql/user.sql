17749c703471f859fcc60887df019095f53899bf
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-27 13:44:42 +0100

    done sql

--- server/sql/user.sql
+++ server/sql/user.sql
@@ -1,8 +1,9 @@
-CREATE TABLE user_db (
-  id INT IDENTITY(1,1) PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  password_hash VARCHAR(128) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `user_db` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password_hash` varchar(128) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
